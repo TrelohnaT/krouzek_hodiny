14f61b94f8c0ed5bdf70adc6f543ce6413ed57a2
TrelohnaT <48327399+TrelohnaT@users.noreply.github.com>
2023-02-27 18:47:22 +0100

    Add files via upload

---
+++ hodina17/engine/magic/vector.js
@@ -0,0 +1,23 @@
+
+
+class Vector
+{
+    constructor(id, x, y)
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+
+    }
+
+    debug()
+    {
+        console.log("vector: " + this.id);
+        console.log("x: " + this.x);
+        console.log("y: " + this.y);
+
+    }
+
+}
+
+
---
+++ hodina17/myPage/myPage_scripts.js
@@ -0,0 +1,19 @@
+
+let article_text_el = document.getElementById("article_text");
+
+function article_text_show()
+{
+    console.log("article_text_show");
+
+    article_text_el.style.display = "block";
+
+}
+
+function article_text_hide()
+{
+    console.log("article_text_hide");
+
+    article_text_el.style.display = "none";
+
+}
+
